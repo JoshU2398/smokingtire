57807dfbb4de805eafce9b08d3749b42ea287477
Eirik Shackelford <Eshackelford@dev-10.com>
2022-04-28 12:00:51 -0400

    Listing repo interface

--- SmokingTire/src/main/java/learn/capstone/data/ListingRepo.java
+++ SmokingTire/src/main/java/learn/capstone/data/ListingRepo.java
@@ -1,4 +1,22 @@
 package learn.capstone.data;
 
+import learn.capstone.models.Listing;
+
+import java.util.List;
+
 public interface ListingRepo {
+
+    List<Listing> findAllListings();
+
+    List<Listing> findByMakeId(int makeId);
+
+    List<Listing> findByModelId(int modelId);
+
+    List<Listing> findByPriceRange(Integer min, Integer max);
+
+    Listing add(Listing toAdd);
+
+    boolean edit(Listing toEdit);
+
+    boolean deleteById(int listingId);
 }
