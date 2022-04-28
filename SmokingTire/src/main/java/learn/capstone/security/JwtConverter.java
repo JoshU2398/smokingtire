07b82de045b525cbc4c608355c2e94f7e3e8c0b9
Eirik Shackelford <Eshackelford@dev-10.com>
2022-04-28 11:22:32 -0400

    Created security classes and added security dependencies

---
+++ SmokingTire/src/main/java/learn/capstone/security/JwtConverter.java
@@ -0,0 +1,67 @@
+package learn.capstone.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class JwtConverter {
+
+    Key secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public User getUserFromToken(String token ){
+        try {
+            JwtParser parser = Jwts.parserBuilder()
+                    .requireIssuer("smoking-tires")
+                    .setSigningKey(secretKey)
+                    .build();
+
+            Jws<Claims> claims = parser.parseClaimsJws(token.substring(7));
+
+            String username = claims.getBody().getSubject();
+
+            String authorities = (String) claims.getBody().get("authorities");
+
+            String[] authSplit = authorities.split(",");
+
+            List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+
+            for (String auth : authSplit) {
+                grantedAuthorities.add(new SimpleGrantedAuthority(auth));
+            }
+            return new User(username, username, grantedAuthorities);
+
+        } catch( JwtException ex ){
+            ex.printStackTrace( System.err );
+            return null;
+        }
+
+    }
+
+    public String getTokenFromUser( User toConvert ){
+        String commaSeparatedRoles = "";
+
+        for( var authority : toConvert.getAuthorities()  ){
+            commaSeparatedRoles += authority + ",";
+        }
+
+        commaSeparatedRoles = commaSeparatedRoles.substring(0, commaSeparatedRoles.length() - 1);
+
+        return Jwts.builder()
+                .setIssuer("smoking-tires")
+                .setSubject( toConvert.getUsername() )
+                .claim( "authorities", commaSeparatedRoles )
+                .setExpiration( new Date( System.currentTimeMillis() + 15 * 60 * 1000) )
+                .signWith( secretKey )
+                .compact();
+    }
+
+}
