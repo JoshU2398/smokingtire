c51470ab1c187f96024798c94b4899f2f217d40a
unknown <mazam@dev-10.com>
2022-04-29 14:13:09 -0400

    Modified ListingDbRepo

--- SmokingTire/src/test/java/learn/capstone/data/ListingDbRepoTest.java
+++ SmokingTire/src/test/java/learn/capstone/data/ListingDbRepoTest.java
@@ -7,8 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
-        //(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class ListingDbRepoTest {
 
     @Autowired
