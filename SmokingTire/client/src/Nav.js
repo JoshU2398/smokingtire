b279a27c9219158c5c6ed14eda9c72b1f8ae5115
unknown <mazam@dev-10.com>
2022-05-02 16:40:14 -0400

    Added basic components to Front End

---
+++ SmokingTire/client/src/Nav.js
@@ -0,0 +1,38 @@
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import AuthContext from './AuthContext';
+
+
+
+function Nav(){
+    const [user, setUser] = useContext(AuthContext);
+
+    function handleLogout(){
+        localStorage.removeItem("token");
+        setUser(null);
+    }
+
+    return (
+        <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+
+                    {user?.user ? (
+                        <li><button onClick={handleLogout}>Logout {user.user.sub}</button></li>
+                    ) : (
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                    )}
+
+                </ul>
+        </nav>
+    )
+
+
+
+}
+
+export default Nav;
