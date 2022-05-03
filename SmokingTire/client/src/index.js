866679a16eac360196bc1cb4cdba90b40796c948
Josh U <Jundari@dev-10.com>
2022-05-03 14:30:59 -0400

    Login functionality achieved

--- SmokingTire/client/src/index.js
+++ SmokingTire/client/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOMClient from 'react-dom/client'
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 
 const container = document.getElementById('root');
 const root = ReactDOMClient.createRoot(container);
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>);
+  </BrowserRouter>);
 
