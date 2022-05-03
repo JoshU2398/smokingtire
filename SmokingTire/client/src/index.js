8786a3d70051301a4ef79b4e1ad7cab638d7c965
unknown <mazam@dev-10.com>
2022-05-03 12:53:31 -0400

    Changed index.js

--- SmokingTire/client/src/index.js
+++ SmokingTire/client/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOMClient from 'react-dom/client'
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-
-const container = document.getElementById('root');
-const root = ReactDOMClient.createRoot(container);
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>);
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
 
