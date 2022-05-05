3c023bdb141e9addb9705ededb60463be9e3f240
Eirik Shackelford <Eshackelford@dev-10.com>
2022-05-05 10:31:19 -0400

    fixing listing

--- SmokingTire/client/src/Listing.js
+++ SmokingTire/client/src/Listing.js
@@ -1,62 +1,51 @@
 
 import { useContext, useEffect } from "react";
-import { useParams } from "react-router-dom";
-
-
-
-
-
-
-function Listing(props) {
-    const { listingId, listingText, userId, carId, createDate, views, mileage, price, isAvailable } = props.listingObj;
-
-import { useContext } from "react";
+import { useParams, Link, useNavigate } from "react-router-dom";
 import AuthContext from "./AuthContext";
 import DeleteListing from "./DeleteListing";
-import { Link } from "react-router-dom";
 
 function Listing(props){
     const {listingId, description, userId, carId, postDate, viewCount, mileage, price, isAvailable} = props.listingObj;
 
     const [user, setUser] = useContext(AuthContext);
 
+    const nav = useNavigate();
 
+    const { someId } = useParams();
 
     function increaseViewCount() {
-        const { someId } = useParams();
-        useEffect(() => {
-            const jwt = localStorage.getItem("token");
-            if (jwt) {
-                fetch("http://localhost:8080/api/increaseViewCount/" + someId,
-                    {
-                        headers: {
-                            Authorization: "Bearer " + jwt;
-                        }
+ 
+        const jwt = localStorage.getItem("token");
+        if (jwt) {
+            fetch("http://localhost:8080/api/increaseViewCount/" + someId,
+                {
+                    headers: {
+                        Authorization: "Bearer " + jwt
                     }
+                }
 
 
-                )
-                    .then(response => {
-                        if (response.status == 200) {
-                            return response.json();
-                        } else {
-                            console.log(response);
-                            alert("update to view count failed!");
-                        }
-                    })
-                    .then(retrievedViews => {
-                        console.log(retrievedViews);
-                        setToEdit();
-                    })
-                    .catch(rejection => {
-                        console.log(rejection);
-                        alert("Rejected!")
-                    });
+            )
+                .then(response => {
+                    if (response.status == 200) {
+                        return response.json();
+                    } else {
+                        console.log(response);
+                        alert("update to view count failed!");
+                    }
+                })
+                .then(retrievedViews => {
+                    console.log(retrievedViews);
+                })
+                .catch(rejection => {
+                    console.log(rejection);
+                    alert("Rejected!")
+                });
 
-            } else {
-                nav();
-            }
-        }, []);
+        } else {
+            nav();
+        }
+
 
         var viewContainer = document.querySelector(".webiste-counter");
         var viewCount = localStorage.getItem("page_view");
@@ -72,11 +61,6 @@ function Listing(props){
 
 
         viewContainer.innerHTML = viewCount;
-
-
-    }
-
-    function convertToPurchased() {
 
 
     }
