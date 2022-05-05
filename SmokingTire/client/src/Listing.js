16a3d548d82c3e93b3eea273705beaa6b391a321
unknown <mazam@dev-10.com>
2022-05-05 00:46:42 -0400

    Added IncreaseViewCount and ConvertToPurchase functions to Listing component

--- SmokingTire/client/src/Listing.js
+++ SmokingTire/client/src/Listing.js
@@ -1,13 +1,75 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 
 
 
 
 
-function Listing(props){
-    const {listingId, listingText, userId, carId, createDate, views, mileage, price, isAvailable} = props.listingObj;
+function Listing(props) {
+    const { listingId, listingText, userId, carId, createDate, views, mileage, price, isAvailable } = props.listingObj;
     const [user, setUser] = useContext(AuthContext);
+
+
+
+    function increaseViewCount() {
+        const { someId } = useParams();
+        useEffect(() => {
+            const jwt = localStorage.getItem("token");
+            if (jwt) {
+                fetch("http://localhost:8080/api/increaseViewCount/" + someId,
+                    {
+                        headers: {
+                            Authorization: "Bearer " + jwt;
+                        }
+                    }
+
+
+                )
+                    .then(response => {
+                        if (response.status == 200) {
+                            return response.json();
+                        } else {
+                            console.log(response);
+                            alert("update to view count failed!");
+                        }
+                    })
+                    .then(retrievedViews => {
+                        console.log(retrievedViews);
+                        setToEdit();
+                    })
+                    .catch(rejection => {
+                        console.log(rejection);
+                        alert("Rejected!")
+                    });
+
+            } else {
+                nav();
+            }
+        }, []);
+
+        var viewContainer = document.querySelector(".webiste-counter");
+        var viewCount = localStorage.getItem("page_view");
+
+        if (viewCount) {
+            viewCount = Number(viewCount) + 1;
+            localStorage.setItem("page_view", viewCount);
+        } else {
+
+            viewCount = 1;
+            localStorage.setItem("page_view", 1);
+        }
+
+
+        viewContainer.innerHTML = viewCount;
+
+
+    }
+
+    function convertToPurchased() {
+
+
+    }
 
     return (
         <div className="listing-item">
@@ -21,8 +83,8 @@ function Listing(props){
             <p>Created: {createDate}</p>
             {user?.user.sub === userId || user?.user.authorities.includes("USER") ? (
                 <>
-                <Link to={'/edit/' + listingId}>Edit</Link>
-                <DeleteListing listingId={listingId} removeFromState={props.removeFromState}/>
+                    <Link to={'/edit/' + listingId}>Edit</Link>
+                    <DeleteListing listingId={listingId} removeFromState={props.removeFromState} />
                 </>
             ) : (
                 null
