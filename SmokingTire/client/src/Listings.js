1f4431a9a9014ea823a94b372a40b6b124f64a58
unknown <mazam@dev-10.com>
2022-05-10 16:40:53 -0400

    Added more to css

--- SmokingTire/client/src/Listings.js
+++ SmokingTire/client/src/Listings.js
@@ -1,42 +1,46 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 
-function Listings(){
+function Listings() {
     const [listings, setListings] = useState([]);
 
     useEffect(() => {
         fetch("http://localhost:8080/api/listings/findAvailable")
-        .then(response => {
-            if(response.status === 200){
+            .then(response => {
+                if (response.status === 200) {
                     return response.json();
-            }else{
-                alert("Something went wrong while fetching...");
-            }
-        })
-        .then(listingData => setListings(listingData))
-        .catch(rejection => {
-            alert("Failure: " + rejection.status + ": " + rejection.statusText)
-        });
+                } else {
+                    alert("Something went wrong while fetching...");
+                }
+            })
+            .then(listingData => setListings(listingData))
+            .catch(rejection => {
+                alert("Failure: " + rejection.status + ": " + rejection.statusText)
+            });
     }, []);
 
-    function removeListingFromState(listingId){
+    function removeListingFromState(listingId) {
         setListings(listings.filter(listing => listing.listingId !== listingId));
 
     }
 
-    function listingFactory(){
-        return listings.map(listing => <Listing
-        key={listing.listingId}
-        listingObj={listing}
-        removeFromState={removeListingFromState}
-        />);
+    function listingFactory() {
+        return listings.map(listing =>
+            <div className="col-4">
+                <Listing
+                    key={listing.listingId}
+                    listingObj={listing}
+                    removeFromState={removeListingFromState}
+                />
+            </div>
+        );
     }
 
 
     return (
         <>
-        
-        {listingFactory()}
+
+            {listingFactory()}
         </>
     )
 }
